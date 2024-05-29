b57b207b0aa1ca09258d0be0644a7d02e79a7fb1
KimInteger <kjs20151240@gmail.com>
2024-05-29 12:45:29 +0900

    내가 생각하는 서버 기본 모듈초기화 및 서버를 포트에 연결

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('node:http');
+
+const fs = require('node:fs');
+
+const path = require('node:path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req,res)=>{
+
+});
+
+server.listen(PORT,(err)=>{
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("서버가동중");
+    console.log(`http://localhost:${PORT}`);
+  }
+})
